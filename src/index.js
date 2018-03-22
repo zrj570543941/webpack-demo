1790b2c05454cb0aefc6b2aa122dc9e8b7cc3878
zrj570543941 <570543941@qq.com>
2018-03-22 18:28:07 +0800

    split webpack config into tow parts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import component from "./component";
+// const merge = require("webpack-merge");
 document.body.appendChild(component());
+
+// console.log(merge(
+//     [{a: 1, c: {d:3}}, {d: 3}],
+//     [{b: 2}]
+// ));
